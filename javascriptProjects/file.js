7c2eea1dbe0f027ecab7d5f817d9f3dc690450c6
KUMAR_HARSH <kh143307@gmail.com>
2022-06-23 23:48:51 +0530

    added js file,included stuff in index and main

---
+++ javascriptProjects/file.js
@@ -0,0 +1,35 @@
+//import "./gamestyle.css";
+
+var a= "hello world";
+console.log(a);
+var b= 20;
+console.log(b);
+var c = true;
+console.log(c);
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
+var d = null;
+console.log(d);
+console.log(typeof null);
+
+
+var z= 20;
+var x = "20";
+console.log(z==x);
+console.log(z===x);
+
+var group = [1,2,3,"hello world",4];
+console.log(group);
+
+var exp =[1,"hello world",function() {console.log('fu');}];
+
+console.log(exp[2]);
+console.log(typeof(typeof(z)));
+
+//type coersion
+
+console.log(typeof(2+"1"));
+"1"+2
+console.log(2+4+5+6+7+"9"+2+3+4+5+6+2);
